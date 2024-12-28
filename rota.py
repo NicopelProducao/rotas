397d965230a661f0c916d01331f08fece1ef746f
NicopelProducao <167545404+NicopelProducao@users.noreply.github.com>
2024-12-28 07:55:31 -0300

    d

--- rota.py
+++ rota.py
@@ -183,7 +183,7 @@ def gerar_pdf(df_filtered, frete_tipo, semana,  cidades, motorista, veiculo ):
     # Adicionar informações de filtro no cabeçalho
     pdf.set_font("Arial", size=8)
     pdf.cell(80, 5, txt=f"Semana: {semana if semana else 'Não especificado'}", ln=False)
-    pdf.cell(80, 5, txt=f"Tipo de Frete: {frete_tipo if frete_tipo else 'Não especificado'}", ln=True)
+    pdf.cell(80, 5, txt=f"Tipo de Frete: {frete_tipo if frete_tipo else 'Não especificado'}", ln=False)
     pdf.cell(80, 5, txt=f"Cidades: {cidades if cidades else 'Não especificado'}", ln=False)
     pdf.cell(80, 5, txt=f"Motorista: {motorista if motorista else 'Não especificado'}", ln=True)
     pdf.cell(80, 5, txt=f"Veiculo: {veiculo if veiculo else 'Não especificado'}", ln=False)
