31c024bcf68a54d88f107621158854e88cc87285
NicopelProducao <167545404+NicopelProducao@users.noreply.github.com>
2024-12-28 08:51:26 -0300

    f

--- rota.py
+++ rota.py
@@ -199,10 +199,10 @@ def gerar_pdf(df_filtered, frete_tipo, semana,  cidades, dia,  motorista, veicul
     # Cabeçalho da tabela
     pdf.cell(15, 5, txt="Pedido", border=1, align="C", fill=True)  # Aplicando a cor de fundo
     pdf.cell(60, 5, txt="Cliente", border=1, align="C", fill=True)
-    pdf.cell(60, 5, txt="Item", border=1, align="C", fill=True)
+    pdf.cell(70, 5, txt="Item", border=1, align="C", fill=True)
     pdf.cell(17, 5, txt="Qtd", border=1, align="C", fill=True)
     pdf.cell(21, 5, txt="Conferência", border=1, align="C", fill=True)  # Nova coluna "Conferência"
-    pdf.cell(21, 5, txt="Faltante", border=1, align="C", fill=True)      # Nova coluna "Faltante"
+      # Nova coluna "Faltante"
     pdf.ln(5)  # Adiciona uma linha abaixo
     pdf.set_text_color(0, 0, 0)
     # Inicializar variável para o pedido anterior
@@ -230,7 +230,7 @@ def gerar_pdf(df_filtered, frete_tipo, semana,  cidades, dia,  motorista, veicul
 
    
         pdf.cell(60, 5, txt=texto_concatenado, border=1, align="L", fill=True if row['color'] == 0 else False)
-        pdf.cell(60, 5, txt=str(row['Descrição Item Faturamento']), border=1, align="C", fill=True if row['color'] == 0 else False)
+        pdf.cell(70, 5, txt=str(row['Descrição Item Faturamento']), border=1, align="C", fill=True if row['color'] == 0 else False)
         pdf.cell(17, 5, txt=str(row['Qtd']), border=1, align="C", fill=True if row['color'] == 0 else False)
         pdf.cell(21, 5, txt="", border=1, align="C", fill=True if row['color'] == 0 else False)  # Conferência (vazio)
     
